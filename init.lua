9166bb28c9a8a6f82cc26cf464df00a7beb211bc
Lenny Truong <leonard@truong.io>
2024-01-25 11:26:39 -0800

    Update neovim config

--- init.lua
+++ init.lua
@@ -18,8 +18,97 @@ require("lazy").setup({
    tag = '0.1.5',
    dependencies = { 'nvim-lua/plenary.nvim' },
    lazy = false},
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {"catppuccin/nvim", name = "catppuccin", priority = 1000},
+  {"nvim-treesitter/nvim-treesitter", 
+   build = ":TSUpdate",
+   config = function ()
+     local configs = require("nvim-treesitter.configs")
+     configs.setup({
+       ensure_installed = { "c", "lua", "vim", "vimdoc", "python", "verilog", "mlir" },
+       sync_install = false,
+       auto_install = true,
+       highlight = { enable = true },
+       indent = { enable = true }
+     })
+   end},
+  {"hrsh7th/nvim-cmp",
+   event = "InsertEnter",
+   dependencies = {
+     "hrsh7th/cmp-cmdline",
+     "hrsh7th/cmp-path",
+     "hrsh7th/cmp-buffer",
+     "hrsh7th/cmp-nvim-lsp",
+     "hrsh7th/nvim-lspconfig",
+     "L3MON4D3/LuaSnip",
+     "saadparwaiz1/cmp_luasnip"
+   },
+   config = function ()
+     local cmp = require("cmp")
+     cmp.setup({
+       snippet = {
+         expand = function(args)
+           require('luasnip').lsp_expand(args.body)
+         end,
+       },
+       mapping = cmp.mapping.preset.insert({
+         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+         ['<C-f>'] = cmp.mapping.scroll_docs(4),
+         ['<C-Space>'] = cmp.mapping.complete(),
+         ['<C-e>'] = cmp.mapping.abort(),
+
+         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+       }),
+       sources = cmp.config.sources({
+         { name = 'nvim_lsp' },
+
+         { name = 'vsnip' }, -- For vsnip users.
+         -- { name = 'luasnip' }, -- For luasnip users.
+         -- { name = 'ultisnips' }, -- For ultisnips users.
+         -- { name = 'snippy' }, -- For snippy users.
+       }, {
+
+         { name = 'buffer' },
+       })
+     })
+
+     -- Set configuration for specific filetype.
+     cmp.setup.filetype('gitcommit', {
+       sources = cmp.config.sources({
+         { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+       }, {
+
+         { name = 'buffer' },
+       })
+     })
+
+     -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+     cmp.setup.cmdline({ '/', '?' }, {
+
+       mapping = cmp.mapping.preset.cmdline(),
+
+       sources = {
+         { name = 'buffer' }
+       }
+     })
+
+     -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+     cmp.setup.cmdline(':', {
+       mapping = cmp.mapping.preset.cmdline(),
+       sources = cmp.config.sources({
+         { name = 'path' }
+       }, {
+         { name = 'cmdline' }
+       })
+     })
+
+     -- Set up lspconfig.
+     local capabilities = require('cmp_nvim_lsp').default_capabilities()
+     -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+     require('lspconfig')['pylsp'].setup {}
+   end
+  }
 })
+
 
 vim.cmd.colorscheme "catppuccin"
 
@@ -38,3 +127,45 @@ vim.api.nvim_create_autocmd(
   {pattern={'gitcommit', 'gitrebase', 'gitconfig'},
    command=[[set bufhidden=delete]]}
 )
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
