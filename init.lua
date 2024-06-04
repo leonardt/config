a2ecbf3167bc57246c99ceb3fed3664b91d0d7b8
Lenny Truong <lenny.truong@sifive.com>
2024-06-04 12:02:16 -0700

    Update indent logic

--- init.lua
+++ init.lua
@@ -124,9 +124,15 @@ require("lazy").setup({
 
 vim.opt.autoindent = true
 vim.opt.smartindent = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.api.nvim_create_autocmd(
+  'Filetype', {
+    pattern = { 'python' },
+    command = 'setlocal tabstop=4 softtabstop=4 shiftwidth=4'
+  }
+)
 vim.opt.smarttab = true
 vim.opt.expandtab = true
 
